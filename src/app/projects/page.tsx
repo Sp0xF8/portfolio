27b5fa45034f6aa59b8b28a82c2675d91c9a711f
Sp0xF8 <spoonnosaurus@gmail.com>
2024-03-19 12:23:19 +0000

    Working query push

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,32 @@
 import Link from "next/link";
 
+import { useEffect, useState } from "react";
 
-export default function Projects() {
+import { query } from "@/app/lib/dbconnect";
+
+import { getProjects, getProjectById } from "@/app/lib/dbhandler";
+
+
+
+export default async function Projects() {
+
+	const projects = await query('SELECT * FROM projects');
+	console.log(projects[0].projectName);
+
+	const project = await getProjectById(1);
+
+	console.log(project);
+
+	
+
+
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1 className="text-6xl font-bold">Sp0xF8's Projects</h1>
 			<Link href="/">Home</Link>
+
+			
 		</main>
 
 	);
