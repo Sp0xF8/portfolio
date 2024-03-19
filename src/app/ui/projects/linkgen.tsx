ecf9f0da07da365cabd2cdc99b081a0d4defab8f
Sp0xF8 <spoonnosaurus@gmail.com>
2024-03-19 18:31:28 +0000

    Updated styling and components- moved project creation to dynamic form in tablist

--- src/app/ui/projects/linkgen.tsx
+++ src/app/ui/projects/linkgen.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
+import { CircularFilledAddition } from "@/app/ui/svgs/icons";
 
+import styles from '@/app/ui/projects/navbar.module.css';
 
 
 
@@ -52,6 +54,53 @@ export const PopulateNav: React.FC<PopulateNavProps> = ({ projectInJson }) => {
 
 				return (
 					<>
+
+						<div>
+							<button id="new_project_btn" className="flex-none h-10 w-full bg-gray-900 flex items-center justify-center text-2xl font-bold text-orange-500" onClick={
+								() => {
+									var addProjectContainer = document.getElementById('addProjectContainer');
+									var addProjectButton = document.getElementById('new_project_btn');
+
+									if (addProjectContainer?.classList.contains(styles.AddProject_Hidden)) {
+										addProjectContainer.classList.remove(styles.AddProject_Hidden);
+										addProjectContainer.classList.add(styles.AddProject);
+									} else {
+										addProjectContainer.classList.remove(styles.AddProject);
+										addProjectContainer.classList.add(styles.AddProject_Hidden);
+									}
+								}
+							
+							}>
+								< CircularFilledAddition color="rgb(249 115 22) " width={30} height={30} class_name="mr"/> New Prject
+							</button>
+
+
+							<div id="addProjectContainer" className={styles.AddProject_Hidden}>
+								<div className="text-blue-300 py-2 px-2">
+
+									<form id='newProjectForm' className="flex flex-col gap-2">
+										<label htmlFor="projectName">Project Name</label>
+										<input type="text" id="projectName" name="projectName" placeholder='Project Alpha' className='text-teal-900 w-full' required/>
+
+										<label htmlFor="projectDescription">Project Description</label>
+										<textarea id="projectDescription" name="projectDescription" placeholder='Project Alpha Descriptions ' className='text-teal-900 w-full' required></textarea>
+
+										<label htmlFor="projectURL">Project URL</label>
+										<input type="text" id="projectURL" name="projectURL" placeholder='https://github.com/' className='text-teal-900 w-full' required/>
+
+										<button type="submit" className="bg-gray-950 text-blue-300 p-2 rounded-md w-full mb-2">Submit Project</button>
+									</form>
+
+									
+
+								</div>
+
+								
+
+							</div>
+						</div>
+
+						
 					
 						
 					
